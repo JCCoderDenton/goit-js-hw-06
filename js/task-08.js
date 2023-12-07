dab247747206a85ee084cf4fcbafec762725fa54
JCCoderDenton <denys_m99@ukr.net>
2023-12-07 02:42:07 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,11 +1,9 @@
 const btn = document.querySelector('button[type="submit"]').addEventListener("click", loginFuction);
 let form = document.forms[0];
 function loginFuction(){
-  const login = document.querySelector('input[type="email"]');
-  const password = document.querySelector('input[type="password"]');
   let email = form.elements.email;
   let pass = form.elements.password;
-  if (login.value == null || login.value == "" || password.value == null || password.value == "")
+  if (email.value == null || email.value == "" || pass.value == null || pass.value == "")
   window.alert("Введіть Email та пароль!");
   else{
     const obj = new Object();
