67ebad94dc6446a70951eac0a2e5d67bbbbc5fb0
JCCoderDenton <denys_m99@ukr.net>
2023-12-09 14:22:51 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -3,20 +3,7 @@ const categories = Array.from(document.querySelectorAll('li.item'));
 
 console.log("Number of categories: ",categories.length);  
 
-categories.forEach(function(element, i) {
-    categories[i].childNodes.forEach(function(element, k) {
-        let numberElements = 0;
-        if (categories[i].childNodes[k].localName == "h2" ){
-            console.log("Category: ",categories[i].childNodes[k].innerText);  
-        }
-        if (categories[i].childNodes[k].localName == "ul" ){            
-            categories[i].childNodes[k].childNodes.forEach(function(element, j) {
-                if (categories[i].childNodes[k].childNodes[j].localName == "li" ){
-                    numberElements++;  
-                }
-            });
-            console.log("Elements: ",numberElements);
-            //console.log("Elements: ",categories[i].childNodes[k].childElementCount);    //Получення кількості childNodes через DOM
-        }
-    });
+categories.forEach(function(element, k) {
+    console.log("Category: ",categories[k].childNodes[1].innerText);  
+    console.log("Elements: ",categories[k].childNodes[3].childElementCount);
 });
